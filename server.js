eefabe249c87a686f74f1802b849117693c17e96
Rooper1337 <julian.seiler@freenet.de>
2022-01-03 18:11:05 +0100

    test bsc scan api

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
-console.log("start app");
+import bscscan, { account } from 'bsc-scan'
 
+
+//bscscan.setUrl('https://testnet.bscscan.com')
+bscscan.default.setApiKey('API_KEY')
+
+console.log(bscscan);
+
+const start = async () => {
+  try {
+    const transactions = await account.getTransactions('0x41cf3e9534156405a133cda545af9ff0e586500a');
+
+    console.log("Transaktionen: " + transactions);
+
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+start()
 
