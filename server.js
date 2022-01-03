e13dbf8aa3b36372c2a7abf0ae7d0f1104655735
OneClickPablo <92474595+OneClickPablo123@users.noreply.github.com>
2022-01-03 20:10:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,8 @@
-import bscscan, { account } from 'bsc-scan'
+ import web3
 
+    web3.bsc.Bsc(pub_key, priv_key)
+    w = web3.Web3()
 
-//bscscan.setUrl('https://testnet.bscscan.com')
-bscscan.default.setApiKey('API_KEY')
+    w.eth.getBalance('0x2910543af39aba0cd09dbb2d50200b3e800a63d2');
 
-console.log(bscscan);
-
-const start = async () => {
-  try {
-    const transactions = await account.getInternalTransactionsByAdress('0x41cf3e9534156405a133cda545af9ff0e586500a');
-
-    console.log("Transaktionen: " + transactions);
-
-  } catch (err) {
-    console.log(err)
-  }
-}
-
-start()
 
